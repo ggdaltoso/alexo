f4c9b5a230118c4ec3a7e1e7e31b64ee6284c19b
ggdaltoso <ggdaltoso@gmail.com>
2025-04-16 20:32:53 -0300

    refactor: optimize BatteryIndicator component by using useMemo for icon rendering

--- src/components/BatteryIndicator.tsx
+++ src/components/BatteryIndicator.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Battery, BatteryCharging } from 'lucide-react';
+import React, { useMemo } from 'react';
+import { Battery } from 'lucide-react';
 import { useBattery } from '../hooks/useBattery';
+import { Systray300, Systray301, Systray302, Systray306 } from '@react95/icons';
 
 export const BatteryIndicator = React.memo(() => {
   const { batteryStatus, error } = useBattery();
@@ -14,13 +15,24 @@ export const BatteryIndicator = React.memo(() => {
     );
   }
 
+  const icon = useMemo(() => {
+    if (batteryStatus.charging) {
+      return <Systray306 variant="32x32_4" />;
+    }
+    if (batteryStatus.level < 30) {
+      return <Systray302 variant="32x32_4" />;
+    } else if (batteryStatus.level < 70) {
+      return <Systray301 variant="32x32_4" />;
+    } else if (batteryStatus.level <= 100) {
+      return <Systray300 variant="32x32_4" />;
+    }
+
+    return null;
+  }, [batteryStatus]);
+
   return (
-    <div className="flex items-center space-x-2 bg-white rounded-3xl w-full justify-between">
-      {batteryStatus.charging ? (
-        <BatteryCharging className="size-4 text-green-500" />
-      ) : (
-        <Battery className="size-4 text-gray-600" />
-      )}
+    <div className="space-x-1 float-right inline-flex items-center">
+      {icon}
       <span className="text-xs font-medium">{batteryStatus.level}%</span>
     </div>
   );
