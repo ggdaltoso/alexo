37d15fc415e7ba787fa3647f86ace35584989018
ggdaltoso <ggdaltoso@gmail.com>
2025-12-15 22:01:16 -0300

    feat: add Clock component for displaying time in São Paulo timezone
    This commit introduces a new Clock component that utilizes the PixelatedClock and a custom hook to display the current time in the São Paulo timezone. The component is designed to be responsive and visually appealing.

---
+++ frontend/src/screens/Clock.tsx
@@ -0,0 +1,23 @@
+import PixelatedClock from '../components/PixelatedClock';
+import { useTimeZone } from '../hooks/useTimeZone';
+
+const SAO_PAULO_TZ = { label: 'São Paulo', timeZone: 'America/Sao_Paulo' };
+
+export const Clock = () => {
+  const time = useTimeZone(SAO_PAULO_TZ.timeZone);
+
+  return (
+    <div className="flex items-center justify-center my-auto h-full gap-2">
+      <PixelatedClock size={220} date={time} />
+      <div className="block text-4xl font-bold">
+        {time.toLocaleTimeString('en-US', {
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: false,
+        })}
+      </div>
+    </div>
+  );
+};
+
+Clock.displayName = 'Clock';
