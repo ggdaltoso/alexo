31242409e0de3f359754155479d37162815d2ac6
ggdaltoso <ggdaltoso@gmail.com>
2025-12-15 22:01:11 -0300

    feat: refactor App and ForecastDashboard components
    - Updated App component to use a loading state and display a loader while fetching weather data.
    - Replaced CurrentWeather component with Clock in the routing.
    - Refactored ForecastDashboard to improve structure and added weather data display.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
-import ForecastDashboard from './screens/ForecastDashboard';
+import { ForecastDashboard } from './screens/ForecastDashboard';
 import CalendarScreen from './screens/CalendarScreen';
 import MessageScreen from './screens/MessageScreen';
-import { CurrentWeather } from './screens/CurrentWeather';
+import { Clock } from './screens/Clock';
+import { useApp } from './contexts';
+import { Loader } from 'lucide-react';
 
 export default function App() {
+  const { weatherLoading: loading } = useApp();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <Loader className="w-8 h-8 animate-spin text-gray-600" />
+      </div>
+    );
+  }
+
   return (
     <Routes>
-      <Route path="/" element={<CurrentWeather />} />
+      <Route path="/" element={<Clock />} />
       <Route path="/forecast" element={<ForecastDashboard />} />
       <Route path="/calendar" element={<CalendarScreen />} />
       <Route path="/message" element={<MessageScreen />} />
